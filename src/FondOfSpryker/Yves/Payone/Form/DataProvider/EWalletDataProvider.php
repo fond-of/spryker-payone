a35fd6834b1b0c7a20591de61d9d7bdff1d949ee
Daniel Rose <daniel-rose@gmx.de>
2018-11-27 14:45:54 +0100

    bugfix for core module

--- src/FondOfSpryker/Yves/Payone/Form/DataProvider/EWalletDataProvider.php
+++ src/FondOfSpryker/Yves/Payone/Form/DataProvider/EWalletDataProvider.php
@@ -2,6 +2,10 @@
 
 namespace FondOfSpryker\Yves\Payone\Form\DataProvider;
 
+use Generated\Shared\Transfer\PaymentTransfer;
+use Generated\Shared\Transfer\PayonePaymentEWalletTransfer;
+use Generated\Shared\Transfer\PayonePaymentTransfer;
+use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
 use SprykerEco\Shared\Payone\PayoneApiConstants;
 use SprykerEco\Yves\Payone\Form\DataProvider\EWalletDataProvider as SprykerEcoEWalletDataProvider;
 
@@ -15,5 +19,22 @@ class EWalletDataProvider extends SprykerEcoEWalletDataProvider
         return [
             'PayPal' => PayoneApiConstants::E_WALLET_TYPE_PAYPAL,
         ];
+    }
+
+    /**
+     * @param \Spryker\Shared\Kernel\Transfer\AbstractTransfer|\Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Spryker\Shared\Kernel\Transfer\AbstractTransfer
+     */
+    public function getData(AbstractTransfer $quoteTransfer)
+    {
+        if ($quoteTransfer->getPayment() === null) {
+            $paymentTransfer = new PaymentTransfer();
+            $paymentTransfer->setPayone(new PayonePaymentTransfer());
+            $paymentTransfer->setPayoneEWallet(new PayonePaymentEWalletTransfer());
+            $quoteTransfer->setPayment($paymentTransfer);
+        }
+
+        return $quoteTransfer;
     }
 }
